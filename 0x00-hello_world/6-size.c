1abfc5d5f179e2f2aa35366908786c7126e3a2db
olutola oluronbi <oluronbiolutola@example.com>
2023-08-13 20:21:37 -0700

    not working properly

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - main function to print size of
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	printf("size of char: %lu byte(s)", sizeof(char));
+	printf("size of int: %lu byte(s)", sizeof(int));
+	printf("size of a long int: %lu byte(s)", sizeof(long int));
+	printf("size of a long long int: %lu byte(s)", sizeof(long long int));
+	printf("size of float: %lu byte(s)", sizeof(float));
+	return (0);
+}
