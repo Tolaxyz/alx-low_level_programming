50202ac8d57357f5220573126a023d14da1d4f47
olutola oluronbi <oluronbiolutola@example.com>
2023-08-13 19:30:03 -0700

    line over 80 characters long

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - functin that prints the value with proper grammar, but the outcome is a piece of art,
+ * Description - the function above print the value listed
+ * Return: always 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
