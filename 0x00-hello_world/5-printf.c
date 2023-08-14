2f35fe78705282eb834e8a1f4c4f490420733c2a
olutola oluronbi <oluronbiolutola@example.com>
2023-08-13 19:38:58 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-
 /**
- * main - functin that prints the value with proper grammar, but the outcome is a piece of art,
- * Description - the function above print the value listed
+ * main - main function
+ *
  * Return: always 0
- */
+*/
 int main(void)
+
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
